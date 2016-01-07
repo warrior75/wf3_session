01855d52936c3d41534cd461a8364093c18f9c74
warrior75 <waryor75@gmail.com>
2016-01-07 10:25:34 +0100

    wf3_session v 1.0

--- Logout.php
+++ Logout.php
@@ -1,6 +1,9 @@
 <?php 
+
 	// même pour effacer une variable en session, on doit utiliser session_start()
 	session_start();
+	include(__DIR__.'include/nav.php');
+	$page = 'logout';
 
 	// supprime la variable SUPER GLOBALE $_SESSION
 	unset($_SESSION['user']);
@@ -8,6 +11,6 @@
 	//Création d'un message de déconnexion
 	$_SESSION['message']="Vous avez été déconnecté du service";
 
-	header('Location: index.php');
+	header('Location: home.php');
 	die();
  ?>
